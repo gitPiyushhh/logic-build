5417bb69fe498f753036198c9b118dbf7805ad82
gitPiyushhh <Piyushhhagarwal@gmail.com>
2022-02-15 11:21:58 +0530

    First commit

---
+++ arrays/game_of_thrones.py
@@ -0,0 +1,10 @@
+##########################################
+######### 1️⃣ Game of thrones
+
+'''1. Map all: 
+    keys: All characters present, values: No. of times charcter present
+    
+    2. We can only allow if the 1 character with odd frequency
+    
+    3. Print the characters half no. me even frequency wale, mid me odd frequency, right me half even frequency wale'''
+
